cf7faee377ef56f004ce7394c2aa30f88f4e0f3c
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-26 14:30:50 -0300

    Test domain Class-Categoria

--- src/main/java/com/resources/CategoriaResource.java
+++ src/main/java/com/resources/CategoriaResource.java
@@ -1,17 +1,26 @@
 package com.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	
 	@GetMapping
-	public String Listar() {
-		return "Rest Function";
+	public List<Categoria> Listar () {
+		List<Categoria> lista = new ArrayList<>();
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		lista.addAll(Arrays.asList(cat1,cat2));
+		return lista;
 	}
-
+	
 }
