ddfe8f53aa608c68f9b9359326ed41656cf602df
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-26 15:34:55 -0300

    Created Repository and Service for Categoria

---
+++ src/main/java/com/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.services;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.domain.Categoria;
+import com.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
+	}
+
+}
